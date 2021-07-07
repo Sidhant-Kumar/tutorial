80915cccf9fa0f0f42a87f57145120a82f2dc71a
sidhantk11153 <sidhantk11155@gmail.com>
2021-07-07 13:54:55 +0530

    url error persists error: tutorial.quickstart no such module found

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quickstart',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +70,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 10
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
