457113ee8240fbf79805906cc740866fa20f8bda
sidhantk11153 <sidhantk11155@gmail.com>
2021-07-07 10:46:31 +0530

    files created on 7-july-2021

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -7,8 +7,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'username', 'email', 'groups']
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
-
-
     class Meta:
         model = Group
         fields = ['url', 'name']
